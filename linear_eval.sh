cc3bb029aaebb1bdffe85f6889e8509696886fb8
Matthias Otth <matthias.otth@gmail.com>
2023-11-07 15:48:58 +0100

    make scripts use $USER

--- linear_eval.sh
+++ linear_eval.sh
@@ -13,14 +13,14 @@ echo "Starting on:     $(date)"
 echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
 
 nvidia-smi
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name barlow.yaml 
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name byol.yaml 
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name mocov3.yaml 
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name nnclr.yaml 
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name simclr.yaml 
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name vibcreg.yaml 
-# python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name vicreg.yaml 
-python -m pipenv run python /itet-stor/zdavid/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name cl_lin_pred_min.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name barlow.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name byol.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name mocov3.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name nnclr.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name simclr.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name vibcreg.yaml 
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name vicreg.yaml 
+python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_linear.py --config-path scripts/linear/imagenet-100/ --config-name cl_lin_pred_min.yaml 
 
 
 # Send more noteworthy information to the output log
