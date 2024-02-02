30fe79d5da65450aa0ace432d3cb19e5d8a9d367
Matthias Otth <maotth@tik42x.ee.ethz.ch>
2024-02-02 18:02:13 +0100

    Updated train script for cmdline args, updated default config

--- train.sh
+++ train.sh
@@ -5,8 +5,9 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --output=/home/maotth/log/%j.out     # where to store the output (%j is the JOBID), subdirectory "log" must exist
 #SBATCH --error=/home/maotth/log/%j.err  # where to store error messages
-#SBATCH --exclude=tikgpu04,tikgpu05,tikgpu07,tikgpu09
-#SBATCH --constraint='geforce_rtx_3090|rtx_a6000'
+#CommentSBATCH --exclude=tikgpu01,tikgpu02,tikgpu03,tikgpu04
+#CommentSBATCH --constraint='geforce_rtx_3090|rtx_a6000'
+#CommentSBATCH --nodelist=tikgpu09
 # #SBATCH --exclude=tikgpu08,tikgpu09,tikgpu10
 
 #a100_80gb geforce_gtx_titan_x geforce_rtx_2080_ti geforce_rtx_3090 rtx_a6000 tesla_v100 titan_rtx titan_xp
@@ -19,8 +20,8 @@ echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
 nvidia-smi
 
 # python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name barlow_cl_lin_pred_min.yaml
-python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name cl_non_lin_pred_minv6.yaml
-#python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name cl_lin_pred_min_sgd.yaml
+python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name cl_non_lin_pred_minv6.yaml $@
+# python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name cl_lin_pred_min_sgd.yaml
 # python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name cl_lin_pred_min.yaml
 # python -m pipenv run python /itet-stor/$USER/net_scratch/Self-Supervised-Learning-with-Predictability-Minimization/main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name barlow.yaml
 
